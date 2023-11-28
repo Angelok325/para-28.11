a14a226601794c8f2383ef50895093ca06e9628d
4442 <4442@DESKTOP-BT2PDD3>
2023-11-28 19:50:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿class person
+{
+    private string name;
+    private int age;
+    private string status;
+    public person(string name, int age, string status)
+    {
+        this.name = name;
+        this.age = age;
+        this.status = status;
+    }
+    public string Name {
+        get {
+            return name;
+        }
+    }
+    public int Age {
+        get {
+            return age;
+        }
+    }
+    set { age = vaule}
+    public string Status {
+    set {
+        status = value;
+    }
+}
+        }
+
+
+
+
+
+
